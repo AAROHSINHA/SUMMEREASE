5a32d6064aad0596e78b6cdb0a8cb52030da0a90
AAROH_SINHA__ <167206572+AAROHSINHA@users.noreply.github.com>
2025-05-05 23:06:40 +0530

    Update database.js

--- src/database.js
+++ src/database.js
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
-const YOUR_SUPABASE_URL = "https://rheedwavpuhlqvrohvlf.supabase.co";
+const YOUR_SUPABASE_URL = "ADD";
 const YOUR_SUPABASE_ANON_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJoZWVkd2F2cHVobHF2cm9odmxmIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0NTM0NzY1MCwiZXhwIjoyMDYwOTIzNjUwfQ._neZBaE00ucvaHsAzqT0x2edFf0AZYYgRW5TQieyf4c";
+  "ADD";
 
 const supabase = createClient(YOUR_SUPABASE_URL, YOUR_SUPABASE_ANON_KEY);
 
